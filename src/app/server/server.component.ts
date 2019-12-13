9513ad69d35ae66b69671b20af8c06815c56a9c0
KyrieWangNYU <kw2116@nyu.edu>
2019-12-13 11:34:41 -0800

    correction

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../../shared/dish.model';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-server',
@@ -7,6 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  found = false;
+  dishName: string;
+  ingredient : string;
+
+  dishes : Dish[] = [new Dish('tomato', 'egg', 'TomatoEgg'),
+                     new Dish('spicy', 'meat', 'SpicyMeat')]
+  search(){
+    for (let i = 0; i < this.dishes.length; i++){
+      if (this.ingredient === this.dishes[i].first || this.ingredient === this.dishes[i].second){
+        this.dishName = this.dishes[i].result;
+        this.found = true;
+      }
+    }
+  }
+
+  reset(){
+
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ src/shared/dish.model.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by kyrie on 12/13/19.
+ */
+export class Dish {
+  constructor(public first: string, public second: string, public result: string){
+
+  }
+}
